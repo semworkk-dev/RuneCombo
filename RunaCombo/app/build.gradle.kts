ae4c893816ee8dd60f922f665699fec268780f2e
semworkk-dev <semworkk@gmail.com>
2026-06-16 12:46:01 +1000

    Update build.gradle.kts

--- RunaCombo/app/build.gradle.kts
+++ RunaCombo/app/build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
+id("com.android.application")
+id("org.jetbrains.kotlin.android")
+id("kotlin-kapt")
+id("com.google.dagger.hilt.android")
 }
+
 
 android {
     namespace = "com.runacombo"
